fb767875393f8d40e50dca5689168659f662d24c
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-05 20:38:37 +0400

    Make models migrations

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='name')
+    preview = models.ImageField(**NULLABLE, verbose_name='image')
+    description = models.TextField(verbose_name='description')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'course'
+        verbose_name_plural = 'courses'
+        ordering = ('name',)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name='name')
+    preview = models.ImageField(**NULLABLE, verbose_name='image')
+    description = models.TextField(verbose_name='description')
+    video_url = models.TextField(verbose_name='video_url')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'lesson'
+        verbose_name_plural = 'lessons'
+        ordering = ('name',)
