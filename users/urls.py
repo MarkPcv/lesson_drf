5a3eccc29a3baa38a7ba7a7a4df59cd54a529985
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-05 21:22:15 +0400

    Realise CRUD DRF for user model using ViewSets

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
+from rest_framework import routers
+
 from users.apps import UsersConfig
+from users.views import UserViewSet
 
 app_name = UsersConfig.name
 
-urlpatterns = [
-]
+router = routers.DefaultRouter()
+router.register('users', UserViewSet, basename='users')
+
+urlpatterns = router.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ViewSet):
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
