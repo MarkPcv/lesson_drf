6e6f1b3f76b4a5389e0116a663fcc64efd030a72
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-05 20:58:57 +0400

    Realise CRUD DRF for lesson model using Generics

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
+
 from courses.apps import CoursesConfig
+from courses.views import LessonListAPIView, LessonCreateAPIView, \
+    LessonRetrieveAPIView, LessonUpdateAPIView
 
 app_name = CoursesConfig.name
 
 urlpatterns = [
-
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/<int:pk>/delete/', LessonUpdateAPIView.as_view(), name='lesson-delete'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from courses.models import Lesson
+from courses.serializers import CourseSerializer, LessonSerializer
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
