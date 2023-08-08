36485ee20ddb6174ee623e9aaef28067a3403869
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-08 16:58:40 +0400

    Add limited fields for user detail view

--- users/serializers.py
+++ users/serializers.py
@@ -14,3 +14,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class UserLimitedSerializer(serializers.ModelSerializer):
+    """
+    Serializer for :model:`users.User` that limits access for other users
+    """
+    class Meta:
+        model = User
+        exclude = ('password', 'last_name')
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserLimitedSerializer
 
 
 class UserViewSet(viewsets.ViewSet):
@@ -12,6 +13,7 @@ class UserViewSet(viewsets.ViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
     def retrieve(self, request, pk=None):
         """
@@ -19,7 +21,10 @@ class UserViewSet(viewsets.ViewSet):
         """
         queryset = User.objects.all()
         user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
+        if user == request.user:
+            serializer = UserSerializer(user)
+        else:
+            serializer = UserLimitedSerializer(user)
         return Response(serializer.data)
 
 
