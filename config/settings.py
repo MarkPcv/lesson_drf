bf438caa92aa9b9a2e2138f8130dd83152ad1e1c
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-19 21:41:54 +0300

    Add periodic task using celery beat

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGS_ROOT = BASE_DIR / 'logs'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
